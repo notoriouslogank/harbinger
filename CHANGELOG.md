42e9b55c02caffa22d6ac7393c61bbe8100fdae3
notoriouslogank <logan.aker1989@gmail.com>
2023-11-04 01:42:01 -0400

    Deprecated online command; removed log msgs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,25 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.8.5] - 2023-04-11
+
+## Deprecated
+- Command online() replaced with status()
+
+### Changed
+- Formatted main.py using Black formatter
+
+### Bugfixes
+- Removed all log messages containing variables (may not be callable?)
+
+## [0.8.4] - 2023-04-11
+
+### Changed
+- Temporarily disable automatic versioning due to possible bug(s)
+
+### Bugfixes
+- Removed most log messages to attempt to alleviate error(s)
+
 ## [0.8.3] - 2023-04-11
 
 ### Added
--- main.py
+++ main.py
@@ -7,17 +7,19 @@ from dotenv import load_dotenv
 from discord.ext import commands
 from datetime import datetime
 
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 load_dotenv()
 
+
 def getVer():
-    with open('CHANGELOG.md', 'r') as f:
+    with open("CHANGELOG.md", "r") as f:
         changes = f.readlines()
         vLine = changes[6]
         version = vLine[4:9]
         return version
 
-TOKEN = os.getenv('TOKEN')
+
+TOKEN = os.getenv("TOKEN")
 VERSION = getVer()
 sTime = datetime.now()
 
@@ -25,113 +27,109 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 
 @bot.event
 async def on_ready():
-    logging.INFO(f'Bot logged in and listening as {bot.user}.')
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
     print(sTime)
-    print('---------')
+    print("---------")
+
 
 @bot.command()
-async def online(ctx):
-    logging.DEBUG(f'User: {ctx.message.author} Command: online Time: {datetime.now()}')
-    await ctx.send(f'{bot.user} is online.')
+async def status(ctx):
+    await ctx.send(f"{bot.user} is online.")
+
 
 @bot.command()
 async def add(ctx, left: int, right: int):
-    logging.DEBUG(f'User: {ctx.message.author} Command: add Time: {datetime.now()}')
-    logging.DEBUG(f'Output: {left + right}')
     await ctx.send(left + right)
+
 
 @bot.command()
 async def roll(ctx, dice: str):
-    logging.DEBUG(f'User: {ctx.message.author} Command: roll({dice}) Time: {datetime.now()}')
     try:
-        rolls, limit = map(int, dice.split('d'))
+        rolls, limit = map(int, dice.split("d"))
     except Exception:
-        logging.WARN(f'Incorrect format: {dice}')
-        await ctx.send('Format must be NdN!')
-        return    
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    logging.INFO(f'User: {ctx.message.author}')
-    logging.INFO(f'Result: {result}')
+        logging.warn(f"Bad format: {dice}")
+        await ctx.send("Format must be NdN!")
+        return
+    result = ", ".join(str(random.randint(1, limit)) for r in range(rolls))
     await ctx.send(result)
+
 
 @bot.command()
 async def joined(ctx, member: discord.Member):
-    logging.INFO(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
-    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+    await ctx.send(f"{member.name} joined {discord.utils.format_dt(member.joined_at)}")
+
 
 @bot.command()
 async def info(ctx):
-    logging.DEBUG(f'User: {ctx.message.author} Command: info()')
     cTime = datetime.now()
     delta = cTime - sTime
-    embedVar = discord.Embed(title='mcswitch', color=0x00ff00)
-    embedVar.add_field(name='version', value=f'v{VERSION}', inline=True)
-    embedVar.add_field(name='uptime', value=f'{delta}', inline=True)
-    embedVar.add_field(name='author', value='notoriouslogank', inline=True)
-    embedVar.add_field(name='source code', value='https://github.com/notoriouslogank/mcswitch')
+    embedVar = discord.Embed(title="mcswitch", color=0x00FF00)
+    embedVar.add_field(name="version", value=f"v{VERSION}", inline=True)
+    embedVar.add_field(name="uptime", value=f"{delta}", inline=True)
+    embedVar.add_field(name="author", value="notoriouslogank", inline=True)
+    embedVar.add_field(
+        name="source code", value="https://github.com/notoriouslogank/mcswitch"
+    )
     await ctx.send(embed=embedVar)
+
 
 @bot.command()
 async def ping(ctx):
-    logging.DEBUG(f'User: {ctx.message.author} Command: ping()')
-    logging.INFO('Ping: {0}'.format(round(bot.latency, 2)))
-    await ctx.send('Current ping: {0}'.format(round(bot.latency, 2)))
+    await ctx.send("Current ping: {0}".format(round(bot.latency, 2)))
+
 
 @bot.command()
 async def uptime(ctx):
-    logging.DEBUG(f'User: {ctx.message.author} Command: uptime()')
     cTime = datetime.now()
     delta = cTime - sTime
-    logging.INFO(f'Uptime: {delta}')
-    await ctx.send(f'Uptime: {delta}')
+    await ctx.send(f"Uptime: {delta}")
+
 
 @bot.command()
 async def shutdown(ctx):
-    logging.DEBUG(f'User: {ctx.message.author} Command: shutdown()')
-    embedShutdown = discord.Embed(title='Shutdown', color=0xff0000, timestamp=datetime.now())
-    embedShutdown.add_field(name='user', value=f'{ctx.message.author}', inline=True)
-    logging.WARN(f'Shutting down!')
-    await ctx.send(embed = embedShutdown)
+    embedShutdown = discord.Embed(
+        title="Shutdown", color=0xFF0000, timestamp=datetime.now()
+    )
+    embedShutdown.add_field(name="user", value=f"{ctx.message.author}", inline=True)
+    logging.warn(f"Shutting down!")
+    await ctx.send(embed=embedShutdown)
     sys.exit()
+
 
 @bot.command()
 async def mcstart(ctx):
-    await os.system('sh mc.sh')
-    print('Starting the server, maybe.')
-    await ctx.send('Starting MC Server.')
+    await os.system("sh mc.sh")
+    print("Starting the server, maybe.")
+    await ctx.send("Starting MC Server.")
+
 
 @bot.command()
 async def rps(ctx, choice):
-    logging.DEBUG(f'User: {ctx.message.author} Command: rps({choice})')
-    choices = ['rock', 'paper', 'scissors']
+    choices = ["rock", "paper", "scissors"]
     botChoice = choices[random.randint(0, 2)]
-    embedRPS = discord.Embed(color=0x0000ff, title='rock, paper, scissors')
-    embedRPS.add_field(name='You', value=f'{choice}', inline=True)
-    embedRPS.add_field(name='Bot', value=f'{botChoice}', inline=True)
+    embedRPS = discord.Embed(color=0x0000FF, title="rock, paper, scissors")
+    embedRPS.add_field(name="You", value=f"{choice}", inline=True)
+    embedRPS.add_field(name="Bot", value=f"{botChoice}", inline=True)
     if choice == botChoice:
-        embedRPS.add_field(name='result', value='You tied!', inline=False)
-        logging.DEBUG(f'Player: {choice} Bot: {botChoice} Result: Tied')
+        embedRPS.add_field(name="result", value="You tied!", inline=False)
         await ctx.send(embed=embedRPS)
-    elif botChoice == 'rock' and choice == 'paper':
-        embedRPS.add_field(name='result', value='You win!', inline=False)
-        logging.DEBUG(f'Player: {choice} Bot: {botChoice} Result: Win')
+    elif botChoice == "rock" and choice == "paper":
+        embedRPS.add_field(name="result", value="You win!", inline=False)
         await ctx.send(embed=embedRPS)
-    elif botChoice == 'paper' and choice == 'scissors':
-        embedRPS.add_field(name='result', value='You win!', inline=False)
-        logging.DEBUG(f'Player: {choice} Bot: {botChoice} Result: Win')
+    elif botChoice == "paper" and choice == "scissors":
+        embedRPS.add_field(name="result", value="You win!", inline=False)
         await ctx.send(embed=embedRPS)
-    elif botChoice == 'scissors' and choice == 'rock':
-        embedRPS.add_field(name='result', value='You win!', inline=False)
-        logging.DEBUG(f'Player: {choice} Bot: {botChoice} Result: Win')
+    elif botChoice == "scissors" and choice == "rock":
+        embedRPS.add_field(name="result", value="You win!", inline=False)
         await ctx.send(embed=embedRPS)
     else:
-        embedRPS.add_field(name='result', value='You lose!', inline=False)
-        logging.DEBUG(f'Player: {choice} Bot: {botChoice} Result: Lose')
+        embedRPS.add_field(name="result", value="You lose!", inline=False)
         await ctx.send(embed=embedRPS)
+
 
 bot.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
