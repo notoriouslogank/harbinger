628197aa2891e90c28e8531341d1fe7f69fcde24
logank <logan.aker1989@gmail.com>
2023-11-07 14:27:50 -0500

    Still tryinng to fix cogs

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ channel = bot.get_channel(CHANNEL)
 async def load_cogs():
     for f in os.listdir("./cogs"):
         if f.endswith(".py"):
-            bot.load_extension("cogs." + f[:-3])
+            await bot.load_extension("cogs." + f[:-3])
             print(f'loading cogs.{f[:-3]}')
 
 async def send_dm(ctx, member: discord.Member, *, content):
