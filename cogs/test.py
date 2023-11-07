595e0dc3998c3106b9cdd63bfce0bf227bda0734
logank <logan.aker1989@gmail.com>
2023-11-07 14:40:53 -0500

    changed function of cogs.test to try to test functionality

--- cogs/test.py
+++ cogs/test.py
@@ -5,16 +5,10 @@ from discord.ext import commands
 class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self._last_member = None
         
     @commands.command()
-    async def test(self, ctx, *, member: discord.Member = None):
-        member = member or ctx.author
-        if self._last_member is None or self._last_member.id != member.id:
-            ctx.send(f'Hello, {member.name}!')
-        else:
-            ctx.send(f'Hello, {member.name}. This feels familiar...')
-        self._last_member = member
+    async def test(self, ctx):
+        await ctx.send('pass')
         
 def setup(bot):
     bot.add_cog(Test(bot))
