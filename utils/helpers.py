54f03e61aeef0ffd90ef59149ad3ffd99961b475
logank <logan.aker1989@gmail.com>
2023-11-09 10:51:54 -0500

    still trying different vars to make fabric work

--- utils/helpers.py
+++ utils/helpers.py
@@ -55,7 +55,7 @@ def timestamp():
 
 def mcStart():
     ssh = Connection('logank@mimir')
-    sshMimir = ssh.run('tmux -c "java -Xms6G -Xmx6G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper-1.20.2-280.jar --nogui"')
+    sshMimir = ssh.run("tmux -c 'bash /home/logank/paper-1.0.0-dev/startup.sh'")
     return sshMimir
 
 async def send_dm(ctx, member: discord.Member, *, content):
