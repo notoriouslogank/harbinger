3f73753fbfe685b3f0a788900d01cf70902373ff
logank <logan.aker1989@gmail.com>
2023-11-10 10:13:44 -0500

    Fixed formatting; added docstrings

--- utils/serverAgent.py
+++ utils/serverAgent.py
@@ -1,42 +1,59 @@
 from fabric import Connection, transfer
 from helpers import Helpers
 
-mc_hostname = Helpers.get_mc_host()
 # startup_script = /home/logank/paper-test/java.sh <<< This should probably be included in the repo
 
-class ServerAgent():
-    
-    agent = Connection(f'{mc_hostname}')
-    
-    def __init__(self, agent):
-        self.agent = agent
-        
-    def ipServer(agent):
-        serverIp = agent.run("curl https://ipinfo.io/ip > /home/logank/paper-test/ip.txt")
-        getIp = transfer.Transfer(agent).get(remote="/home/logank/paper-test/ip.txt")
 
-    
-    def startServer(agent):
+class ServerAgent:
+    """A class to represent the SSH agent managing the server/client connection.
+
+    Attributes
+    ----------
+    agent: Class_method
+        the instance of the server agent
+
+    Methods
+    -------
+    get_server_ip(agent):
+        run curl on remote server to get public IP
+    start_server(agent):
+
+    stop_server(agent):
+
+    command_server(agent):
+    """
+
+    mc_hostname = Helpers.get_mc_host()
+    agent = Connection(f"{mc_hostname}")
+
+    def __init__(self, agent):
+        """Constructs necessary paramenters for initializing the agent."""
+        self.agent = agent
+
+    def get_server_ip(agent):
+        """Run curl on remote and send text file back to local."""
+        query_ip = agent.run(
+            "curl https://ipinfo.io/ip > /home/logank/paper-test/ip.txt"
+        )
+        get_ip = transfer.Transfer(agent).get(remote="/home/logank/paper-test/ip.txt")
+
+    def start_server(agent):
         """Create an SSH connection and start the Minecraft server."""
-        serverSetup = agent.agent(
-        "tmux new -d -s server"
-        )  # These can probably be pared down into one long command
-        serverDir = agent.run('tmux send -t server:0 "cd /home/logank/paper-test" C-m')
-        serverStart = agent.run('tmux send -t server:0 "./java.sh" C-m')
+        create_tmux = agent.agent("tmux new -d -s server")
+        cd_to_dir = agent.run('tmux send -t server:0 "cd /home/logank/paper-test" C-m')
+        server_start = agent.run('tmux send -t server:0 "./java.sh" C-m')
         print("Starting the server...")
 
-
-    def stopServer(agent):
+    def stop_server(agent):
         """Runs the /stop command in the Minecraft server."""
-        serverStop = agent.run('tmux send -t server:0 "stop" C-m')
+        server_stop = agent.run('tmux send -t server:0 "stop" C-m')
         print("Stopping the server...")
 
-
-    def commandServer(command: str, agent):
+    def command_server(command: str, agent):
         """Send a command to the Minecraft server via tmux.
 
         Args:
             command (str): Minecraft server command to send
         """
-        serverCommand = agent.run(f'tmux send -t server:0 "{command}" C-m')
+        server_command = agent.run(f'tmux send -t server:0 "{command}" C-m')
         print(f"Sent command: {command}.")
