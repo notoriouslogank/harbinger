da490a71a7e7104f541b81de4309c32f4067fe72
logank <logan.aker1989@gmail.com>
2023-11-10 15:37:05 -0500

    trying to make logging more efficient if possible

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import logging
 from utils.helpers import Helpers
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s'))
+logger.addHandler(handler)
 
 sTime = Helpers.sTime
 bot = Helpers.bot
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,18 +1,9 @@
-import logging
-
 from datetime import datetime
 from os import getenv
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s'))
-logger.addHandler(handler)
-
 
 class Helpers:
     """Class to help integrate modules together.
