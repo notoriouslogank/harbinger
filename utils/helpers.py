e5bb2cbf4224afe3005ab72a6e2f53e98f0ca77b
logank <logan.aker1989@gmail.com>
2023-11-10 15:35:17 -0500

    testing a new logging strat

--- utils/helpers.py
+++ utils/helpers.py
@@ -7,6 +7,12 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s'))
+logger.addHandler(handler)
+
 
 class Helpers:
     """Class to help integrate modules together.
