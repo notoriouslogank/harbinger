5506ab472e6eeab62f0a0b6f5c2af647be458dd5
logank <logan.aker1989@gmail.com>
2023-11-09 22:27:29 -0500

    Updated README.md with better information.

--- utils/mcswitch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-ssh logank@mimir -t tmux new -d -s server
-ssh logank@mimir -t 'tmux send -t server:0 "cd /home/logank/paper-test" C-m'
-ssh logank@mimir -t 'tmux send -t server:0 "./java.sh" C-m'
