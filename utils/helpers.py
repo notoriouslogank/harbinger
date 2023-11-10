6dcb0095a55e381a3df2f782852622bcfca8da3c
logank <logan.aker1989@gmail.com>
2023-11-10 15:30:16 -0500

    still just working on logging

--- utils/helpers.py
+++ utils/helpers.py
@@ -7,6 +7,7 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+
 class Helpers:
     """Class to help integrate modules together.
 
@@ -33,13 +34,6 @@ class Helpers:
     send_dm(ctx, member, *, content):
         Send a DM to a given user.
     """
-
-
-    logger = logging.getLogger('discord')
-    logger.setLevel(logging.DEBUG)
-    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s'))
-    logger.addHandler(handler)
 
     cogs = "cogs.moderation", "cogs.status", "cogs.help", "cogs.tools"
     color1 = 0x884EA0
