0e94cf4b7d69d93e5a15a18c0cd01817ed33a342
logank <logan.aker1989@gmail.com>
2023-11-17 23:37:48 -0500

    Cleaned up fixed ping commmand.

--- cogs/status.py
+++ cogs/status.py
@@ -52,10 +52,11 @@ class Status(commands.Cog):
     @commands.command()
     async def ping(self, ctx: commands.Context):
         """Check network latency."""
+        ping = (round(self.bot.latency, 1)) * 1000
         cmd = f"!ping"
         cmd_msg = "Pong!"
         Mcswitch.timestamp(ctx.message.author, cmd, cmd_msg)
-        await ctx.send('Pong! {0}'.format(round(self.bot.latency, 1)))
+        await ctx.send(f'Pong! ({ping})')
 
     @commands.command()
     async def uptime(self, ctx: commands.Context):
