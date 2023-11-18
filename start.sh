7d4b7553805cc3f261a4dc319cfea94dced430e9
logank <logan.aker1989@gmail.com>
2023-11-18 11:22:45 -0500

    swapping panes is fucked

--- start.sh
+++ start.sh
@@ -15,10 +15,10 @@
 tmux new -d -s harbinger
 tmux send -t harbinger:0 "source .venv/bin/activate" C-m
 tmux split-window
-tmux send -t harbinger:0 "ssh logank@mimir" C-m
-tmux swap-pane
 tmux send -t harbinger:0 "python3 -m pip install -r requirements.txt" C-m
 tmux send -t harbinger:0 "python3 harbinger.py" C-m
+tmux swap-pane
+tmux send -t harbinger:0 "ssh logank@mimir" C-m
 
 
 
