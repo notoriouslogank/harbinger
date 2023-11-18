13b93ac7d7714bf9caaa37cd6fe2a8680cc1a30a
notoriouslogank <logan.aker1989@gmail.com>
2023-11-18 10:18:15 -0500

    Created default_config.ini

--- utils/config_writer.py
+++ utils/config_writer.py
@@ -1,9 +1,9 @@
 import configparser
 
 config = configparser.ConfigParser()
-config['Bot'] = {'token': 'token_value', 'channel': 'channel_value', 'custom_color': 0x88ae00}
-config['Paths'] = {'server_dir': '/home/logank/logan_mc_server/try2/', 'startup_script': './java.sh'}
-config['Server'] = {'hostname': 'logank@mimir', 'IP': '192.168.0.1'}
+config['Bot'] = {'token': '', 'channel': '', 'custom_color': ''}
+config['Paths'] = {'server_dir': '', 'startup_script': ''}
+config['Server'] = {'hostname': '', 'IP': ''}
 
-with open("config.ini", 'w') as configfile:
+with open("default_config.ini", 'w') as configfile:
     config.write(configfile)
