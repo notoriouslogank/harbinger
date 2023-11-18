120d18d1fc34b2772654e35f1b698c921b038c60
logank <logan.aker1989@gmail.com>
2023-11-18 11:30:16 -0500

    Start MC server from start.sh

--- start.sh
+++ start.sh
@@ -12,3 +12,4 @@ tmux send -t harbinger:0 "python3 harbinger.py" C-m # Run the bot
 tmux split-window # Split the window; switch panes
 tmux send -t harbinger:0 "ssh logank@mimir" C-m # SSH into mimir
 tmux send -t harbinger:0 "cd /home/logank/logank_mc_server/try2" C-m # cd into server dir
+tmux send -t harbinger:0 "zsh ./java.sh" C-m # Start Minecraft server, in theory
