b7fde07253e49914db183a2058a9b53f916de6f3
logank <logan.aker1989@gmail.com>
2023-11-18 11:19:49 -0500

    sigh

--- start.sh
+++ start.sh
@@ -15,6 +15,7 @@
 tmux new -d -s harbinger
 tmux send -t harbinger:0 "source .venv/bin/activate" C-m
 tmux split-window
+tmux swap-pane
 tmux send -t harbinger:0 "python3 -m pip install -r requirements.txt" C-m
 tmux send -t harbinger:0 "python3 harbinger.py" C-m
 tmux swap-pane
