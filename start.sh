2165d7e57b79a97c5f42edf643577afb935019c4
thenotoriouslogank <121781342+notoriouslogank@users.noreply.github.com>
2023-11-18 11:32:43 -0500

    V1.6.3 dev (#51)
    * Remove mc_server.py as it was unused.
    
    * Trying to make start.sh work as expected.
    
    * Still trying to make start.sh work as expected.
    
    * And still trying to make start.sh work as expected.
    
    * Just trying to make tmux sork right.
    
    * sigh
    
    * sigh
    
    * mayhap this will work
    
    * swapping panes is fucked
    
    * how bout now?
    
    * Fixed tmux settings in start.sh hopefully
    
    * Start MC server from start.sh
    
    ---------
    
    Co-authored-by: logank <logan.aker1989@gmail.com>

--- start.sh
+++ start.sh
@@ -4,18 +4,12 @@
 
 # Navigate to server dir
 # Ensure we have server.jar and paper.jar
-# Ensure we have java.sh
-# Tmux into two windows; start harbinger.py on one
 
-
-
-
-### Basic Config - Uses .venv
-
-tmux new -d -s harbinger
-tmux split-window
-tmux send -t harbinger:0 "source .venv/bin/activate" C-m
-tmux split-window
-tmux send -t harbinger:0 "python3 -m pip install -r requirements.txt" C-m
-tmux send -t harbinger:0 "python3 harbinger.py" C-m
-
+tmux new -d -s harbinger # New tmux session
+tmux send -t harbinger:0 "source .venv/bin/activate" C-m # Source the .venv
+tmux send -t harbinger:0 "python3 -m pip install -r requirements.txt" C-m # Install requirements.txt
+tmux send -t harbinger:0 "python3 harbinger.py" C-m # Run the bot
+tmux split-window # Split the window; switch panes
+tmux send -t harbinger:0 "ssh logank@mimir" C-m # SSH into mimir
+tmux send -t harbinger:0 "cd /home/logank/logank_mc_server/try2" C-m # cd into server dir
+tmux send -t harbinger:0 "zsh ./java.sh" C-m # Start Minecraft server, in theory
