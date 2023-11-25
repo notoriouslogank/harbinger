ab4698a201e6d17067e8ee01d2ffac631cfad59a
logank <logan.aker1989@gmail.com>
2023-11-24 20:52:45 -0500

    fixed formatting

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 source start.config
 
-python3 -m venv .venv                               # Create Python venv
-source .venv/bin/activate                           # Activate .venv
-pip install --upgrade pip                           # Upgrade venv pip
-python3 -m pip install -r requirements.txt          # Install requirements
-tmux new -d -s harbinger                            # Create tmux session
-tmux send -t harbinger:0 "python3 harbinger.py" C-m # Start the bot
-tmux split-window                                   # Create server tmux pane
-tmux send -t harbinger:0 "cd $ServerDir" C-m # Navigate to server dir
-tmux send -t harbinger:0 "zsh $ServerStartScript" C-m  # Start the server
-tmux a -t harbinger                                 # Attach to tmux
+python3 -m venv .venv                                 # Create Python venv
+source .venv/bin/activate                             # Activate .venv
+pip install --upgrade pip                             # Upgrade venv pip
+python3 -m pip install -r requirements.txt            # Install requirements
+tmux new -d -s harbinger                              # Create tmux session
+tmux send -t harbinger:0 "python3 harbinger.py" C-m   # Start the bot
+tmux split-window                                     # Create server tmux pane
+tmux send -t harbinger:0 "cd $ServerDir" C-m          # Navigate to server dir
+tmux send -t harbinger:0 "zsh $ServerStartScript" C-m # Start the server
+tmux a -t harbinger                                   # Attach to tmux
