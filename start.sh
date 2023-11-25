f5cd6b43cf1f8a0f6d20f6b754c1cf468ab9bcbf
logank <logan.aker1989@gmail.com>
2023-11-24 20:45:08 -0500

    maybe fix start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source ./start.config
+source start.config
 
 python3 -m venv .venv                               # Create Python venv
 source .venv/bin/activate                           # Activate .venv
