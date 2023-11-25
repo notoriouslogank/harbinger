b927c7dbceced5660bfcf9c6026aeaf45b662fd1
logank <logan.aker1989@gmail.com>
2023-11-24 20:51:36 -0500

    Fixed start.config, maybe

--- start.sh
+++ start.sh
@@ -8,6 +8,6 @@ python3 -m pip install -r requirements.txt          # Install requirements
 tmux new -d -s harbinger                            # Create tmux session
 tmux send -t harbinger:0 "python3 harbinger.py" C-m # Start the bot
 tmux split-window                                   # Create server tmux pane
-tmux send -t harbinger:0 "cd $server-dir" C-m # Navigate to server dir
-tmux send -t harbinger:0 "zsh $server-start-script" C-m  # Start the server
+tmux send -t harbinger:0 "cd $ServerDir" C-m # Navigate to server dir
+tmux send -t harbinger:0 "zsh $ServerStartScript" C-m  # Start the server
 tmux a -t harbinger                                 # Attach to tmux
