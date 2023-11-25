ca68706b4fb836b021a90c90e50a02bb53acb9c4
logank <logan.aker1989@gmail.com>
2023-11-24 23:58:49 -0500

    Hopefully making minecraft commands work.

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -4,20 +4,29 @@ from harbinger import Harbinger
 import subprocess
 
 bot = Harbinger.bot
-color = Harbinger.custom_color
-
+custom_color = Harbinger.custom_color
 
 class Minecraft(commands.Cog):
+    
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-
+        
     @commands.command()
     async def switch(self, ctx: commands.Context, state="on"):
         if state == "on":
-            subprocess.run("zsh ~/SERVER/run.sh")
-            await ctx.send("Server is starting...")
+            subprocess.run('zsh ~/SERVER/run.sh')
+            await ctx.send('Server is starting...')
         elif state == "off":
             subprocess.run('tmux -t harbinger:0 "stop" C-m')
-            await ctx.send("Stopping server...")
+            await ctx.send('Stopping server...')
         else:
-            await ctx.send("Invalid argument.")
+            await ctx.send('Invalid argument.')
+
+    @commands.command()
+    async def mccmd(self, ctx: commands.Context, command):
+        subprocess.run(f'tmux send -t harbinger:0 "{command}" C-m')
+        await ctx.send(f'Sending command: {command} to server...')
+
+async def setup(bot):
+    """Load cog into bot."""
+    await bot.add_cog(Minecraft(bot))
