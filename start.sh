d57d4a5464bd66d4bce232eba262c92b93617d7a
logank <logan.aker1989@gmail.com>
2023-11-25 23:02:38 -0500

    Clear the terminal before launching bot.

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ source .venv/bin/activate                             # Activate .venv
 pip install --upgrade pip                             # Upgrade venv pip
 python3 -m pip install -r requirements.txt            # Install requirements
 tmux new -d -s harbinger                              # Create tmux session
+tmux send -t harbinger:0 "clear" C-m                  # Clear the terminal
 tmux send -t harbinger:0 "python3 harbinger.py" C-m   # Start the bot
 tmux split-window                                     # Create server tmux pane
 tmux send -t harbinger:0 "cd $ServerDir" C-m          # Navigate to server dir
