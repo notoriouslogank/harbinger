d17963e905a575bb76f0a5364e81b806f3237d4c
logank <logan.aker1989@gmail.com>
2023-11-29 16:40:57 -0500

    Add note reading function.

--- cogs/notes.py
+++ cogs/notes.py
@@ -3,11 +3,11 @@ from discord.ext import commands
 from datetime import datetime
 from harbinger import Harbinger
 
+
 class Notes(commands.Cog):
-    
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        
+
     @commands.command()
     async def note(self, ctx: commands.Context, *content):
         note_author = str(ctx.message.author.display_name)
@@ -16,19 +16,21 @@ class Notes(commands.Cog):
         for word in content:
             note_content = note_content + word + " "
         with open(f"user_notes/{note_author}.txt", "a") as n:
-            n.writelines(f"{timestamp}\n{note_content}\n--------------------\n")
+            n.writelines(f"{timestamp}\n\n{note_content}\n--------------------\n")
             n.close()
+
+    @commands.command()
+    async def notes(self, ctx: commands.Context):
+        note_file = f"user_notes/{ctx.message.author.display_name}.txt"
+        with open(note_file, "r") as n:
+            all_notes = n.read()
+        await ctx.send(all_notes)
 
     @commands.command()
     async def cnote(self, ctx: commands.Context, *content):
         # Clear notes
         pass
-    
-    @commands.command()
-    async def notes(self, ctx: commands.Context):
-        # Print notes
-        pass
-    
+
+
 async def setup(bot):
     await bot.add_cog(Notes(bot))
-        
