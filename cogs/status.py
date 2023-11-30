ef8cd77449533d74157429fd1cf0bde4e1bf3e90
logank <logan.aker1989@gmail.com>
2023-11-30 11:13:30 -0500

    Trying to fix bug report not returning whole message.:

--- cogs/status.py
+++ cogs/status.py
@@ -214,7 +214,7 @@ class Status(commands.Cog):
         cmd_msg = f"Sent bug report."
         message = ""
         for word in raw_message:
-            message = message + word + " "
+            message = message + str(word) + " "
         email_address = Harbinger.email_address
         password = Harbinger.email_pass
         email = EmailMessage()
