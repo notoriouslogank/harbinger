bd052d80d30b199e885583f3cc3ed8f3d4d4225c
logank <logan.aker1989@gmail.com>
2023-11-30 17:45:40 -0500

    Fixing configure.py

--- config/configure.py
+++ config/configure.py
@@ -120,11 +120,11 @@ class Configure:
     def get_moderator_role():
         role_id = input("Moderator Role ID: ")
         return role_id
-    
+
     def get_developer_role():
         role_id = input("Developer Role ID: ")
         return role_id
-        
+
     def write_py_config() -> None:
         """Create config.ini."""
         config = configparser.ConfigParser()
@@ -143,8 +143,10 @@ class Configure:
             "server_local_ip": f"{Configure.obscure(Configure.get_local_ip())}",
             "server_public_ip": f"{Configure.obscure(Configure.get_public_ip())}",
         }
-        config['Roles'] = {"moderator": f"{Configure.obscure(Configure.get_moderator_role)}",
-                           "developer": f"{Configure.obscure(Configure.get_developer_role)}"}
+        config["Roles"] = {
+            "moderator": f"{Configure.obscure(Configure.get_moderator_role())}",
+            "developer": f"{Configure.obscure(Configure.get_developer_role())}",
+        }
 
         with open(
             f"{Configure.config_path}{Configure.python_config_file}", "w"
@@ -165,5 +167,6 @@ class Configure:
 
         with open(f"{Configure.config_path}{Configure.shell_config_file}", "w") as conf:
             conf.write(text)
+
 
 Configure.write_py_config()
