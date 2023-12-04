41f43abb90579dfe50eb1663e8e991079c6b24b5
logank <logan.aker1989@gmail.com>
2023-12-04 18:44:36 -0500

    trying....

--- cogs/tools.py
+++ cogs/tools.py
@@ -43,7 +43,7 @@ class Tools(commands.Cog):
         async with aiohttp.ClientSession() as session:
             async with session.get(f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}') as resp:
                 dict_entry = await resp.json()
-                origin = dict_entry[0]["origin"]
+                origin = dict_entry[0]["phonetics"]
                 definition = dict_entry[0]["meanings"][0]["definitions"][0]["definition"]
                 #origin = dict_entry[0]["origin"]
                 print(definition, origin)
