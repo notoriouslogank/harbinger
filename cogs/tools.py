44bbfd10c9f9e2e6bdee4577f693477e569a1ce1
logank <logan.aker1989@gmail.com>
2023-12-04 20:41:02 -0500

    added print statement to debug, still screwy

--- cogs/tools.py
+++ cogs/tools.py
@@ -58,7 +58,7 @@ class Tools(commands.Cog):
                 if phonetics == None:
                     embed=discord.Embed(title=f"{word}", description=f"No phonetic guide available.", color=CUSTOM_COLOR)
                 elif pronunciation == None:
-                    print(pronunciation)
+                    print(f"pronounce: {pronunciation}")
                     embed = discord.Embed(title=f"{word}", description=f"{phonetics}", color=CUSTOM_COLOR)
                 else:
                     embed = discord.Embed(title=f"{word}", description=f"[{phonetics}]({pronunciation})", color=CUSTOM_COLOR)
