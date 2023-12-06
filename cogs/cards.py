a03d1a1e321d005481846681aafa45b6db559efe
logank <logan.aker1989@gmail.com>
2023-12-05 20:19:02 -0500

    Create player hand

--- cogs/cards.py
+++ cogs/cards.py
@@ -12,13 +12,16 @@ class Cards(commands.Cog):
     @commands.command()
     async def blackjack(self, ctx: commands.Context):
         async with aiohttp.ClientSession() as session:
-            async with session.get(f'https://deckofcardsapi.com/api/deck/new/draw/?count=2') as cards_json:
+            async with session.get(f'https://deckofcardsapi.com/api/deck/new/draw/?count=4') as cards_json:
                 await ctx.message.delete()
                 message = await ctx.send("Dealing...")
                 cards = await cards_json.json()
                 card1 = cards["cards"][0]["image"]
                 card2 = cards["cards"][1]["image"]
+                card3 = cards["cards"][2]["image"]
+                card4 = cards["cards"][3]["image"]
                 await ctx.send(f'{card1}\n{card2}')
+                await ctx.message.author.send(f'{card3}\n{card4}')
                 
 async def setup(bot):
     await bot.add_cog(Cards(bot))
