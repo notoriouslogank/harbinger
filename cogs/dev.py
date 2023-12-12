e857a459fbf5a2261b705d152b679eb70807f3be
logank <logan.aker1989@gmail.com>
2023-12-12 08:25:50 -0500

    Bot now reports current version after update.

--- cogs/dev.py
+++ cogs/dev.py
@@ -132,7 +132,7 @@ class Dev(commands.Cog):
         message = await ctx.send("Checking GitHub for updates...")
         await ctx.message.delete()
         subprocess.run(["git", "pull"])
-        await message.edit(content="Bot is up to date!", delete_after=DELETION_TIME)
+        await message.edit(content=f"Bot is now on version {Harbinger.get_ver()}", delete_after=DELETION_TIME)
         Harbinger.timestamp(ctx.message.author, cmd, cmd_msg)
 
         
