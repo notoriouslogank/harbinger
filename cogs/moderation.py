d5079cf4b4be8ed87cf800fd8d76b15c82506270
logank <logan.aker1989@gmail.com>
2024-01-17 12:59:38 -0500

    log oldest first

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -174,7 +174,7 @@ class Moderation(commands.Cog):
         counter = 0
         filename = "log.txt"  # TODO: Make this part of the config.ini
 
-        async for message in ctx.channel.history():
+        async for message in ctx.channel.history(oldest_first=True):
             if author == None:
                 logfile = Path(filename)
                 entry = f"{counter+1} {message.created_at} - {message.author}: {message.content}\n"
