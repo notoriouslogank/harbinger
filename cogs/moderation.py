7277f7127eefed0c93dbb420f4c00bb61841ca67
logank <logan.aker1989@gmail.com>
2024-01-17 17:28:53 -0500

    why u no worky

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -211,7 +211,7 @@ class Moderation(commands.Cog):
         print(message_list)
 
     @commands.command()
-    @commands.has_role(MODERATOR_ROLE_ID)
+    #@commands.has_role(MODERATOR_ROLE_ID)
     async def serverinfo(self, ctx: commands.Context):
         """Create embeds containing server details and member information and send them to the channel."""
         cmd = "!serverinfo"
@@ -246,7 +246,7 @@ class Moderation(commands.Cog):
             await ctx.send(embed=members_embed)
 
     @commands.command()
-    #@commands.has_role(MODERATOR_ROLE_ID)
+    @commands.has_role(MODERATOR_ROLE_ID)
     async def whois(self, ctx: commands.Context, member: discord.Member) -> None:
         """Get detailed information about given member.
 
