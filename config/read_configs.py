a7ed77c8a11c242770c21f6d66d5132a542289d3
logank <logan.aker1989@gmail.com>
2024-01-16 20:19:08 -0500

    imports?

--- config/read_configs.py
+++ config/read_configs.py
@@ -1,7 +1,7 @@
 from configparser import ConfigParser
 from cryptography.fernet import Fernet
 import discord
-from configure import Configure
+from config.configure import Configure
 
 
 configfile = "config/config.ini"
