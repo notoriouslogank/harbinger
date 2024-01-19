c2ff421d5a68f191c67cb5902eea3b8ea2d29b21
logank <logan.aker1989@gmail.com>
2024-01-18 23:44:18 -0500

    fixing display colors, maybe

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -293,8 +293,8 @@ class Moderation(commands.Cog):
             cmd_msg = f"Got whois info for {member}."
             whois_embed = discord.Embed(
                 title=f"{member.display_name}",
-                description=f"{member.raw_status}",
-                color=member.accent_color,
+                description=f"{member.status}",
+                color=int(member.accent_color),
             )
             whois_embed.add_field(name="Role:", value=f"{member.top_role}", inline=True)
             whois_embed.add_field(
