d6cb54ef8e1d4ee89511bcdaee4ac6efa1b75a4f
logank <logan.aker1989@gmail.com>
2024-04-09 21:13:41 -0400

    move to correct directtory

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import subprocess
 from config.read_configs import ReadConfigs as configs
 
 STARTUP_SCRIPT = configs.startup_script()
+SERVER_DIR = configs.server_dir()
 
 start_tmux = ("tmux", "new-session", "-d", "-s", "Harbinger")
 split_window = ("tmux", "split-window", "-v")
@@ -13,6 +14,7 @@ start_harbinger = (
     "python3 harbinger.py",
     "ENTER",
 )
+cd_mc = ("tmux", "send", "-t", "Harbinger.1", f"cd {SERVER_DIR}", "ENTER")
 start_minecraft = ("tmux", "send", "-t", "Harbinger.1", f"{STARTUP_SCRIPT}", "ENTER")
 attach_tmux = ("tmux", "a")
 
