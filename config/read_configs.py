6039ec801c6c781545f590a24333ea40a7a27155
logank <logan.aker1989@gmail.com>
2024-04-10 08:55:12 -0400

    trying to fix server directory config

--- config/read_configs.py
+++ config/read_configs.py
@@ -99,7 +99,8 @@ class ReadConfigs:
         config.read(python_config_file)  # read
         server_directory = config["Server"]["server_dir"]
         Configure.encrypt(python_config_file, Configure.load_key())  # encrypt
-        return server_directory
+        raw_server_directory = repr(server_directory)[1:-1]
+        return raw_server_directory
 
     def startup_script() -> str:
         """Retrieve path to Minecraft server startup script from config.ini.
