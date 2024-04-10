bc83920f57f581c692bd89740609d54c748bfd1d
logank <logan.aker1989@gmail.com>
2024-04-10 08:24:01 -0400

    trying to find version, still, maybe

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -31,7 +31,7 @@ class Minecraft(commands.Cog):
     def get_mc_version(self):
         with open(fname) as f:
             line = f.readlines()
-            version = line[6]
+            version = line[4]
             return version[-6:-1]
 
     @commands.command()
