273808815e84ed1beaf57805a23e071583e8887e
logank <logan.aker1989@gmail.com>
2024-04-10 08:45:04 -0400

    trying to fix paths for minecraft server

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -9,8 +9,10 @@ from harbinger import Harbinger
 CUSTOM_COLOR = configs.custom_color()
 SERVER_PUBLIC_IP = configs.server_public_ip()
 SERVER_STARTUP_SCRIPT = configs.startup_script()
-SERVER_DIR = r"/home/logank/jaylogankminecraft/logs"
-LOG_NAME = r"latest.log"
+SERVER_DIR = configs.server_dir()
+LOG_NAME = r"logs/latest.log"
+# SERVER_DIR = r"/home/logank/jaylogankminecraft/logs"
+# LOG_NAME = r"latest.log"
 fname = os.path.join(SERVER_DIR, LOG_NAME)
 bot = Harbinger.bot
 
@@ -66,8 +68,10 @@ class Minecraft(commands.Cog):
                 description="",
                 color=CUSTOM_COLOR,
             )
-            mc_embed.add_field(name="Client Version", value=f"{self.get_mc_version()}")
-            mc_embed.add_field(name="Server Address", value=f"{SERVER_PUBLIC_IP}")
+            mc_embed.add_field(
+                name="Client Version", value=f"``{self.get_mc_version()}``"
+            )
+            mc_embed.add_field(name="Server Address", value=f"``{SERVER_PUBLIC_IP}``")
             await ctx.send(embed=mc_embed)
             Harbinger.timestamp(ctx.message.author, cmd, cmd_msg)
         else:
@@ -78,54 +82,6 @@ class Minecraft(commands.Cog):
             stdout = self.get_cmd_stdout()
             await ctx.send(f"``{stdout}``")
             Harbinger.timestamp(ctx.message.author, cmd, cmd_msg)
-
-
-"""     def create_embed(version):
-        Create an embed with the Minecraft server information.
-
-        Args:
-            version (str): The Minecraft version of the current server instance.
-
-        Returns:
-            embed: An embed object containing the Minecraft server version and IP address.
-
-        minecraft_embed = discord.Embed(title="Minecraft", description=f"{version}")
-        minecraft_embed.add_field(name="Server Address", value=f"{SERVER_PUBLIC_IP}")
-        return minecraft_embed
-
-    @commands.command()
-    async def switch(self, ctx: commands.Context, state="on"):
-        Toggle the state of the Minecraft server.
-
-        Args:
-            state (str, optional): Whether to turn the server on or off. Defaults to "on".
-
-        cmd = f"!switch({state})"
-        cmd_msg = f"Switched Minecraft server {state}."
-        embed = Minecraft.create_embed("v1.20.1")
-        if state == "on":
-            subprocess.run(
-                [
-                    "tmux",
-                    "send",
-                    "-t",
-                    "harbinger:0",
-                    f"zsh {SERVER_STARTUP_SCRIPT}",
-                    "C-m",
-                ]
-            )
-            await ctx.send("Server is starting...")
-            await ctx.send(embed=embed)
-            Harbinger.timestamp(ctx.message.author, cmd, cmd_msg)
-        elif state == "off":
-            subprocess.run(["tmux", "send", "-t", "harbinger:1", "/stop", "C-m"])
-            await ctx.send("Stopping server...")
-            Harbinger.timestamp(ctx.message.author, cmd, cmd_msg)
-        else:
-            await ctx.send("Invalid argument.")
-
-
- """
 
 
 async def setup(bot):
