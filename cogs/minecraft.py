6e5e5906b5301c5dd3a4d5cfffcae99ca39281cc
logank <logan.aker1989@gmail.com>
2024-04-09 21:38:09 -0400

    Trying to get mccmd working again.

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -18,62 +18,8 @@ class Minecraft(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    def create_embed(version):
-        """Create an embed with the Minecraft server information.
-
-        Args:
-            version (str): The Minecraft version of the current server instance.
-
-        Returns:
-            embed: An embed object containing the Minecraft server version and IP address.
-        """
-        minecraft_embed = discord.Embed(title="Minecraft", description=f"{version}")
-        minecraft_embed.add_field(name="Server Address", value=f"{SERVER_PUBLIC_IP}")
-        return minecraft_embed
-
-    @commands.command()
-    async def switch(self, ctx: commands.Context, state="on"):
-        """Toggle the state of the Minecraft server.
-
-        Args:
-            state (str, optional): Whether to turn the server on or off. Defaults to "on".
-        """
-        cmd = f"!switch({state})"
-        cmd_msg = f"Switched Minecraft server {state}."
-        embed = Minecraft.create_embed("v1.20.1")
-        if state == "on":
-            subprocess.run(
-                [
-                    "tmux",
-                    "send",
-                    "-t",
-                    "harbinger:0",
-                    f"zsh {SERVER_STARTUP_SCRIPT}",
-                    "C-m",
-                ]
-            )
-            await ctx.send("Server is starting...")
-            await ctx.send(embed=embed)
-            Harbinger.timestamp(ctx.message.author, cmd, cmd_msg)
-        elif state == "off":
-            subprocess.run(["tmux", "send", "-t", "harbinger:0", "stop", "C-m"])
-            await ctx.send("Stopping server...")
-            Harbinger.timestamp(ctx.message.author, cmd, cmd_msg)
-        else:
-            await ctx.send("Invalid argument.")
-
-    @commands.command()
-    async def mccmd(self, ctx: commands.Context, command: str) -> None:
-        """Send an arbitrary command to the Minecraft server.
-
-        Args:
-            command (str): Command to send to the server.
-        """
-        cmd = f"!mccmd({command})"
-        cmd_msg = f"Sent following command to server: {command}"
-        subprocess.run(["tmux", "send", "-t", "harbinger:0", f"{command}", "C-m"])
-        await ctx.send(f"Sending command: {command} to server...")
-        Harbinger.timestamp(ctx.message.author, cmd, cmd_msg)
+    def mc(self, command):
+        subprocess.run(["tmux", "send", "-t", "Harbinger.0", f"{command}", "ENTER"])
 
 
 async def setup(bot):
