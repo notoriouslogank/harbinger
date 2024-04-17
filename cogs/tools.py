554d5d2ccca2125f578ad9a894ea92b4d7a4f46a
logank <logan.aker1989@gmail.com>
2024-04-17 19:48:18 -0400

    embed colors

--- cogs/tools.py
+++ cogs/tools.py
@@ -30,7 +30,9 @@ class Tools(commands.Cog):
         summary_length = 1999 - len(wiki_title)
         wiki_summary = page.summary[0:summary_length]
         wiki_embed = discord.Embed(
-            title=f"{wiki_title}", description=f"> {wiki_summary}"
+            title=f"{wiki_title}",
+            description=f"`` {wiki_summary}``",
+            color=CUSTOM_COLOR,
         )
         await ctx.send(embed=wiki_embed)
 
