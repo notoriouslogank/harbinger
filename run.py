96ad63c9aa015c69601a482fe166ac47d4cbfd6b
thenotoriouslogank <121781342+notoriouslogank@users.noreply.github.com>
2024-08-22 15:02:02 -0400

    Arma (#151)
    * Created !arma command
    
    * Updated; bumped version.
    
    ---------
    
    Co-authored-by: logank <logan.aker1989@gmail.com>

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ from config.read_configs import ReadConfigs as configs
 STARTUP_SCRIPT = configs.startup_script()
 SERVER_DIR = configs.server_dir()
 
-start_tmux = ("tmux", "new-session", "-d", "-s", "Harbinger")
+start_tmux = ("tmux", "-S", "/var/tmux/Harbinger", "new", "-s", "Harbinger")
+go_to = ("cd", "/var/tmux")
 split_window = ("tmux", "split-window", "-v")
 start_harbinger = (
     "tmux",
@@ -17,10 +18,10 @@ start_harbinger = (
 )
 cd_mc = ("tmux", "send", "-t", "Harbinger.1", f"cd {SERVER_DIR}", "ENTER")
 start_minecraft = ("tmux", "send", "-t", "Harbinger.1", f"{STARTUP_SCRIPT}", "ENTER")
-attach_tmux = ("tmux", "a")
-
+attach_tmux = ("tmux", "-S", "/var/tmux/Harbinger", "att", "-t", "Harbinger")
 commands = [
     start_tmux,
+    go_to,
     split_window,
     start_harbinger,
     cd_mc,
